78dd0f26899e84545d1dfce6bc3afac9c83230db
AlexanderValitalo <vaal23eg@student.ju.se>
2023-09-26 14:05:55 +0200

    first push

---
+++ script.js
@@ -0,0 +1,40 @@
+const logInDiv = document.createElement("div"); //Skapar åtkomst till diven som inloggninselementen ska ligga i
+logInDiv.setAttribute("class", "logInDiv"); //Ger denna div klassen logInDiv
+
+const rulesDiv = document.getElementById("rules"); //Skapar åtkomst till den sista diven 
+document.body.insertBefore(logInDiv, rulesDiv); //Lägger in den nya diven mellan de 2 andra
+
+const logInH2 = document.createElement("h2"); //Skapar variabel till en rubrik för inloggningsdiven
+const logInInputName = document.createElement("input"); //Skapar variabel till ett inputelement för namnet
+const logInInputPassword = document.createElement("input"); //Skapar variabel till ett inputelement för lösenordet
+const logInButton = document.createElement("button"); //Skapar variabel till inloggningsknappen
+
+const namn = "Bella"; //Skapar string med användarnamnet
+const lösenord = "qwe123"; //Skapar string med lösenordet
+
+//Sätter in inställningar på items i diven logInDiv så som, klass, text, placeholder.
+logInH2.innerHTML = "Logga In"; 
+logInH2.setAttribute("class", "logInDiv-item");
+logInInputName.setAttribute("type", "text");
+logInInputName.setAttribute("placeholder", "namn");
+logInInputName.setAttribute("class", "logInDiv-item");
+logInInputPassword.setAttribute("type", "text");
+logInInputPassword.setAttribute("placeholder", "lösenord");
+logInInputPassword.setAttribute("class", "logInDiv-item");
+logInButton.innerHTML = "Logga in";
+logInButton.setAttribute("class", "logInDiv-item");
+
+//Lägger till en eventlyssnare till knappen som inväntar att användaren klickar på den
+logInButton.addEventListener("click", function(){
+    if(logInInputName.value === namn && logInInputPassword.value === lösenord){
+        logInDiv.classList.add("logInDivHide");
+        /***************************************************************************************** */
+    } 
+    
+});
+
+//Lägger in items i logInDiv:en
+logInDiv.appendChild(logInH2);
+logInDiv.appendChild(logInInputName);
+logInDiv.appendChild(logInInputPassword);
+logInDiv.appendChild(logInButton);
